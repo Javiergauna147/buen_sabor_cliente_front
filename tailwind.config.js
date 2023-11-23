f28343c0c0600bec12d75123a035037c4c827a06
DNoblega <diego.noblega@medismart.live>
2023-11-23 08:29:09 -0300

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,scss,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: { 
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans], 
+      }
+    },
+  },
+  plugins: [
+  ],
+}
+
