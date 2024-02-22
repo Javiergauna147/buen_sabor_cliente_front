8507fa7f15da82f1c368a10e4b997701226591b7
Victor Franco <66040727+victorfranco97@users.noreply.github.com>
2024-02-22 17:56:14 -0300

    Cambio de aspecto de card en menu. Agrega vista de quick view

--- src/app/pages/menu-page/menu-page.component.ts
+++ src/app/pages/menu-page/menu-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { Producto, ProductosService } from 'src/app/services/Productos/productos.service';
 import { CarritoService } from 'src/app/services/carrito/carrito.service';
@@ -7,29 +7,47 @@ import { Carrito } from 'src/app/services/localStorageManager/storageCarrito/sto
 @Component({
   selector: 'app-menu-page',
   templateUrl: './menu-page.component.html',
-  styleUrls: ['./menu-page.component.scss']
+  styleUrls: ['./menu-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MenuPageComponent implements OnInit {
 
 
   productos: Producto[] = [];
+  selectedProduct: any;
+  showQuickViewFlag: boolean = false;
 
-  constructor(private ProductoService:ProductosService, private CarritoService:CarritoService, private messageService: MessageService) {}
- 
+  constructor(private ProductoService: ProductosService, private CarritoService: CarritoService, private messageService: MessageService) { }
+
   ngOnInit(): void {
     console.log("MenuPageComponent");
     this.loadProductos();
-    this.CarritoService.carritoUpdated.subscribe((newCarrito:Carrito) => {
+    this.CarritoService.carritoUpdated.subscribe((newCarrito: Carrito) => {
       console.log(newCarrito);
     });
   }
-  
-  async loadProductos(){
+
+  async loadProductos() {
     this.productos = await this.ProductoService.getAllProductosDisponibles();
   }
 
-  selectProduct(id:String){
-    this.CarritoService.addItem(this.productos.find(producto=>producto.id==id)!);
+  selectProduct(event: Event,id: String) {
+    event.stopPropagation(); // Detener la propagación del evento
+    this.CarritoService.addItem(this.productos.find(producto => producto.id == id)!);
     this.messageService.add({ severity: 'success', summary: 'Producto agregado al carrito', detail: '' });
   }
+  removeFromCart(id: String) {
+    console.log('Producto eliminado del carrito ', this.productos.find(producto => producto.id == id)!);
+    this.messageService.add({ severity: 'success', summary: 'Producto eliminado del carrito', detail: '' });
+  }
+  // Método para mostrar el "quick view"
+  showQuickView(producto: any) {
+    this.selectedProduct = producto;
+    this.showQuickViewFlag = true;
+  }
+
+  hideQuickView() {
+    this.showQuickViewFlag = false;
+    this.selectedProduct = null;
+}
 }
